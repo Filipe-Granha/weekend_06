205276ffaa2b555b882c8efe94222efa15f64765
Filipe <granha.carlos@gmail.com>
2017-07-01 21:31:45 +0100

    added totalProfit method, still no tests

--- shop_management/Shop.java
+++ shop_management/Shop.java
@@ -1,3 +1,25 @@
 package shop_management;
+import java.util.ArrayList;
 import behaviours.*; // not sure if needed?
 
+public class Shop {
+  private String name;
+  private ArrayList<Sellable> stock;
+
+  public Shop(String name) {
+    this.name = name;
+    this.stock = new ArrayList<Sellable>();
+  }
+
+
+
+  public int totalPotentialProfit() {
+    int count = 0;
+    for(Sellable items : this.stock) { // I chose 'items', but could have been any other name
+     count += items.calculateMarkup();
+    }
+   return count;
+   }
+
+
+}
